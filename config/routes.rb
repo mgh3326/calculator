8b2c38710a42711bb2d20c61894c49a84f69444e
kwanghyunMoon <mgh3326@naver.com>
2019-12-07 18:56:16 +0900

    Rails 정보 전달하기 실습

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get '/' => 'home#index'
+  post '/result' => 'home#result'
+  get 'plus/:num1/:num2' => 'home#plus'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
